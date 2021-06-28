a55fddfa1a9a7bcbdc41af964890821e75aeade7
JoseLu-Dev <joseludev@gmail.com>
2021-06-28 11:26:45 +0200

    Added Lato font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,23 @@ const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './src/**/*.{html,ts,css,scss,sass,less,styl}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      }
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio')],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
